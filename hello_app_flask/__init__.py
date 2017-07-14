6c8fd39b0b88c780e7eb04861777718d4f411749
Aidan Wong <aw110f@ca02mlaw110f-02.corp.yp.com>
2017-07-13 17:06:34 -0700

    change flask app to return web server hostname and client IP

--- hello_app_flask/__init__.py
+++ hello_app_flask/__init__.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import socket
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    ip = request.remote_addr
+    return "Hello from FLASK. My Hostname is: %s \n Your IP address is: %s \n" % (socket.gethostname(), ip)
 
 @app.route('/imgs')
 def imgs():
     return app.send_static_file('welcome.jpg')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
